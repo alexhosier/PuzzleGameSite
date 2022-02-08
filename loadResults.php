4b1fddefd7f8b3810b9f716e2eb77eb34fe19bb4
Alex Hosier <alexhos75@gmail.com>
2022-02-08 23:23:16 +0000

    Add leaderboard functionality

---
+++ loadResults.php
@@ -0,0 +1,17 @@
+<?php
+    // Include database
+    include "./includes/database.php";
+
+    // SQL Statement
+    $sql = "SELECT * FROM leaderboard ORDER BY play_time ASC LIMIT 25";
+
+    // Fetch the result and process it
+    $result = mysqli_query($conn, $sql);
+
+    $number = 1;
+
+    while($row = $result->fetch_assoc()) {
+        echo '<tr><th scope="row">' . $number . '</th><td>' . $row['play_name'] . '</td><td>' . $row['play_time'] . '</td></tr>';
+        $number = $number + 1;
+    }
+?>
